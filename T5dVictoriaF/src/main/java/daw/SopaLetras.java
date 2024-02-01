db1c975fe24c4631907e87435f150f4d006a77ef
Victoria Fernandez <vfergon346@g.eduacaand.es>
2024-02-01 22:57:24 +0100

    finalizacion del proyecto

--- T5dVictoriaF/src/main/java/daw/SopaLetras.java
+++ T5dVictoriaF/src/main/java/daw/SopaLetras.java
@@ -5,7 +5,6 @@
 package daw;
 
 import java.util.Arrays;
-import java.util.Random;
 import org.apache.commons.lang3.RandomStringUtils;
 
 /**
@@ -13,17 +12,20 @@ import org.apache.commons.lang3.RandomStringUtils;
  * @author vickyfg
  */
 public class SopaLetras {
-    
-    
-    //Matriz NxN
-    private char matriz[][];
-    int n;
-    int m;
 
-    public SopaLetras() {
-        this.matriz = new char[n][m];
+    //Matriz NxN
+    private char[][] matriz;
+    private int n;
+
+    public SopaLetras(int tamaño) {
+        this.n = tamaño;
+        this.matriz = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matriz[i][j] = RandomStringUtils.randomAlphabetic(1, 1).toUpperCase().charAt(0); // Genera un char aleatorio de la A a la Z
+            }
+        }
     }
-       
 
     @Override
     public String toString() {
@@ -54,38 +56,97 @@ public class SopaLetras {
         }
         final SopaLetras other = (SopaLetras) obj;
         return Arrays.deepEquals(this.matriz, other.matriz);
-    }  
-    
-    public SopaLetras(int n){
-        
+    }
+
+    public SopaLetras() {
+
     }
 
     public char[][] getMatriz() {
         return matriz;
     }
-    
+
     // Método para generar una matriz aleatoria de nXn elementos de tipo char
-    public void mostrarMatriz(){
-    for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++){
-                System.out.println("Elemento i, j " + i + j + " : " + 
-                        matriz[i][j]);
+    public void mostrarMatriz() {
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.print("|");
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j]);
+                if (j != matriz[i].length - 1) {
+                    System.out.print("\t");
+                }
             }
+            System.out.println("|");
         }
     }
-    // Método para generar una matriz aleatoria de nXn elementos de tipo char
-    static void generarMatrizAleatoria(int n) {
-        char[][] matriz = new char[n][n];
-        Random random = new Random();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                matriz[i][j] = RandomStringUtils.randomAlphabetic(1, 1).toUpperCase().charAt(0); // Genera un char aleatorio de la A a la Z
+    /*colocarPalabraHorizontal, que recibe una coordenada de la matriz y un String 
+    con la palabra y pone la palabra horizontalmente en la matriz a partir de la 
+    coordenada indicada, si se puede.*/
+    public void colocarPalabraHorizontal(int fila, int columna, String palabra) {
+        if (fila < n && columna < n) {
+            if (palabra.length() <= (matriz.length - columna)) {
+                for (int i = columna; i < (columna + palabra.length()); i++) {
+                        matriz[fila][i] = palabra.charAt(i - columna);                                     
+                }
+            } else {
+                System.out.println("La palabra no tiene un tamaño adecuado");
             }
-        }
+        } else {
+            System.out.println("Elije una posicion adecuada.");
 
-        return matriz;
+        }
+    }
+    /*colocarPalabraHorizontalInvertida,que recibe una coordenada de la matriz y 
+    un String con la palabra y pone la palabra horizontalmente, de forma invertida, 
+    en la matriz a partir de la coordenada indicada, si se puede.*/
+    public void colocarPalabraHorizontalInvertida(int fila, int columna, String palabra) {
+        if (fila < n && columna < n) {
+            if (palabra.length() <= (columna + 1)) {
+                for (int i = columna; i > (columna - palabra.length()); i--) {
+                        matriz[fila][i] = palabra.charAt(columna - i);                                     
+                }
+            } else {
+                System.out.println("La palabra no tiene un tamaño adecuado");
+            }
+        } else {
+            System.out.println("Elije una posicion adecuada.");
+
+        }
     }
     
-    
+    /*colocarPalabraVertical, que recibe una coordenada de la matriz y un String 
+    con la palabra y pone la palabra verticalmente en la matriz a partir de la
+    coordenada indicada, si se puede.*/
+    public void colocarPalabraVertical(int fila, int columna, String palabra) {
+        if (columna < n && fila < n) {
+            if (palabra.length() <= (matriz.length - fila)) {
+                for (int i = fila; i < (fila + palabra.length()); i++) {
+                        matriz[i][columna] = palabra.charAt(i - fila);                                     
+                }
+            } else {
+                System.out.println("La palabra no tiene un tamaño adecuado");
+            }
+        } else {
+            System.out.println("Elije una posicion adecuada.");
+
+        }
+    }
+    /*colocarPalabraVerticalInvertida, que recibe una coordenada de la matriz y 
+    un String con la palabra y pone la palabra verticalmente, de forma invertida, 
+    en la matriz a partir de la coordenada indicada, si se puede.*/
+    public void colocarPalabraVerticalInvertida(int fila, int columna, String palabra) {
+        if (fila < n && columna < n) {
+            if (palabra.length() <= (fila + 1)) {
+                for (int i = fila; i > (fila - palabra.length()); i--) {
+                        matriz[i][columna] = palabra.charAt(fila - i);                                     
+                }
+            } else {
+                System.out.println("La palabra no tiene un tamaño adecuado");
+            }
+        } else {
+            System.out.println("Elije una posicion adecuada.");
+
+        }
+    }
 }
--- T5dVictoriaF/src/main/java/daw/T5dVictoriaF.java
+++ T5dVictoriaF/src/main/java/daw/T5dVictoriaF.java
@@ -12,8 +12,24 @@ public class T5dVictoriaF {
 
     public static void main(String[] args) {
         
-        SopaLetras sopaletras = new SopaLetras();
+       int tamañoSopa = 5;
+        SopaLetras sopaLetras = new SopaLetras(tamañoSopa);
+
+        System.out.println("Sopa de Letras:");
+        sopaLetras.mostrarMatriz();
+        sopaLetras.colocarPalabraHorizontal(1   , 2, "OSO");
+        System.out.println("-----------------HORIZONTAL--------------");
+        sopaLetras.mostrarMatriz();
+        sopaLetras.colocarPalabraHorizontalInvertida(1   , 4, "HOLA");
+        System.out.println("-----------------HORIZONTAL INVERTIDA--------------");
+        sopaLetras.mostrarMatriz();
+        sopaLetras.colocarPalabraVertical(1   , 4, "HOLA");
+        System.out.println("-----------------VERTICAL--------------");
+        sopaLetras.mostrarMatriz();
+        sopaLetras.colocarPalabraVerticalInvertida(4   , 2, "RAIZ");
+        System.out.println("-----------------VERTICAL INVERTIDA--------------");
+        sopaLetras.mostrarMatriz();
         
-        System.out.println(matriz.generarMatrizAleatoria());
     }
+    
 }
