11e55f115de215aaca055ef5f02e27dc3b66fbc8
Victoria Fernandez <vfergon346@g.eduacaand.es>
2024-02-04 23:24:12 +0100

    ParteA hecha

--- T5dVictoriaF/src/main/java/daw/T5dVictoriaF.java
+++ T5dVictoriaF/src/main/java/daw/T5dVictoriaF.java
@@ -17,16 +17,16 @@ public class T5dVictoriaF {
 
         System.out.println("Sopa de Letras:");
         sopaLetras.mostrarMatriz();
-        sopaLetras.colocarPalabraHorizontal(1   , 2, "OSO");
+        sopaLetras.colocarPalabraHorizontal(1, 2, "OSO");
         System.out.println("-----------------HORIZONTAL--------------");
         sopaLetras.mostrarMatriz();
-        sopaLetras.colocarPalabraHorizontalInvertida(1   , 4, "HOLA");
+        sopaLetras.colocarPalabraHorizontalInvertida(1, 4, "HOLA");
         System.out.println("-----------------HORIZONTAL INVERTIDA--------------");
         sopaLetras.mostrarMatriz();
-        sopaLetras.colocarPalabraVertical(1   , 4, "HOLA");
+        sopaLetras.colocarPalabraVertical(1, 4, "HOLA");
         System.out.println("-----------------VERTICAL--------------");
         sopaLetras.mostrarMatriz();
-        sopaLetras.colocarPalabraVerticalInvertida(4   , 2, "RAIZ");
+        sopaLetras.colocarPalabraVerticalInvertida(4, 2, "RAIZ");
         System.out.println("-----------------VERTICAL INVERTIDA--------------");
         sopaLetras.mostrarMatriz();
         
